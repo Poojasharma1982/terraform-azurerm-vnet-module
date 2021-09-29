3a333d7a3ca5c71ded74d7045410ee46ac7761d2
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-29 19:17:58 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "template" {
+  type = string
+  # default = "01000000-0000-4000-8000-000030080200"
+}
+variable "template 2" {
+  type = string
+  default = "01000000-0000-4000-8000-000030080200"
+}
