90ef1be33ac11561bf689f4baa8498f7c06092b6
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-29 22:46:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ resource "azurerm_virtual_network" "myvnet1" {
   name                = var.vnet_name
   location            = "eastus2"
   address_space       = var.vnet_address
+  resource_group_name = var.rg_name
 }
