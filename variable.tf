cfc200a7810bea1bef3f425ac50245d931fb3726
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-29 21:06:12 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,4 @@
-variable "template" {
+variable "virtualNetwork1" {
   type = string
   # default = "01000000-0000-4000-8000-000030080200"
-}
-variable "template 2" {
-  type = string
-  default = "01000000-0000-4000-8000-000030080200"
 }
